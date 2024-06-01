d429d709806644f5abcb816c942d8453cd5e1030
Rodrigo Lavalle Cañizares <rodlavalle@gmail.com>
2024-05-31 22:40:19 -0600

    added full code, README and README demo

---
+++ Atomic_README/README.md
@@ -0,0 +1,79 @@
+
+<h3 align="center">README Reactor 2.0</h3>
+
+<p align="center"><i>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</i></p>
+
+<p align="center">
+<a href="https://github.com/Rod-Freedom/C9-README_Reactor_2.0/blob/main/LICENSE"><img src="https://img.shields.io/github/license/Rod-Freedom/C9-README_Reactor_2.0?style=for-the-badge" alt="License"></a>
+</p>
+
+<br>
+<br>
+
+## **ABOUT THE PROJECT**
+**Overview**
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sodales tortor quis posuere fringilla. Proin rutrum, erat eget posuere pellentesque, ex diam tempor elit, a ultrices tortor orci eget nibh. Aliquam erat volutpat. Vivamus ac arcu arcu.
+
+Find the repo [here](https://github.com/Rod-Freedom/C9-README_Reactor_2.0).
+<br>
+<br>
+
+## Table of Contents
+- [Resources](#resources)
+- [Get Started](#get-started)
+- [Usage](#usage)
+- [License](#license)
+- [Credits](#credits)
+
+<br>
+
+[(Back to the Top)](#about-the-project)
+
+## Resources
+* JavaScript
+* Node.js
+* npm packages
+
+<br>
+
+[(Back to the Top)](#about-the-project)
+
+## Get Started
+Sed vestibulum felis sit amet sem scelerisque hendrerit. Nullam vel velit at ante sodales aliquet.
+
+<br>
+
+[(Back to the Top)](#about-the-project)
+
+## Usage
+Maecenas vestibulum bibendum leo, vel ultricies mauris fringilla ac. Donec pretium, massa non accumsan eleifend, enim est vestibulum enim, ut consequat felis nunc non metus.
+> ![Demo](./assets/SMPTE_Color_Bars.svg.png)
+
+<br>
+
+[(Back to the Top)](#about-the-project)
+
+## License
+![GitHub](https://img.shields.io/github/license/Rod-Freedom/C9-README_Reactor_2.0?style=for-the-badge)<br>
+See the [license](https://github.com/Rod-Freedom/C9-README_Reactor_2.0/blob/main/LICENSE) for more details.
+
+<br>
+
+[(Back to the Top)](#about-the-project)
+
+## Credits
+Developed by [Rod's Freedom.](https://github.com/Rod-Freedom)<br>
+Quisque eget metus elit. Maecenas aliquet mollis nunc.
+
+<br>
+
+[(Back to the Top)](#about-the-project)
+
+<br>
+<br>
+<br>
+
+____
+
+Markdown generated with **[README Reactor 2.0](https://github.com/Rod-Freedom/C9-README_Reactor_2.0)**
--- index.js
+++ index.js
@@ -2,30 +2,230 @@ import fs from 'fs';
 import inquirer from 'inquirer';
 import chalk from 'chalk';
 import chalkAnimation from 'chalk-animation';
-import nanospinner from 'nanospinner';
-import colors from 'colors';
+import { createSpinner } from 'nanospinner';
+import ReadmeObj, { mdReactor } from './utils/mdreactor.js';
+
+const feedReactor = (readmeObj) => {
+    const runReactor = (obj) => {
+        const READMEprod = mdReactor(obj);
+        fs.writeFile('./Atomic_README/README.md', READMEprod, err => {
+            const logo = `
+                                                       
+                        987             398            
+                         907           839             
+                          767         785              
+                           466       453               
+                            56734975723                
+                                ___                    
+              3894902823973    A6 6A    3894902823973  
+                        732   43   78   837            
+                         732   45567   897             
+                           329       653               
+                         232           432             
+                       234               234           
+                     578                   675         
+                                                       
+            `;
+            const thanks = `
+            ......Thanks for using README Reactor......
+            `;
+            const inst = 'Find your atomic README inside the folder with the same name. Careful! It\'s highly radioactive!';
+            
+            if (err) console.log(chalk.redBright('Sorry, an error has been found, please start over.'), err)
+            else {
+                console.log(chalk.cyanBright.italic(inst));
+                console.log(chalk.magentaBright(thanks));
+                const glitchedLogo = chalkAnimation.glitch(logo);
+                setTimeout(() => glitchedLogo.stop(), 5000);
+            }
+        })
+    };
+
+    const getDemo = () => {
+        inquirer
+            .prompt({
+                message: 'Save your demo into the "Atomic_README/assets" folder; it can be a .GIF, .png or .jpg...\nPress ENTER when you\'re done.',
+                name: 'continue'
+            })
+            .then(() => {
+                const filesTesting = fs.readdirSync('./Atomic_README/assets');
+                if (filesTesting.length > 0) {
+                    readmeObj.demo = `> ![Demo](./assets/${filesTesting[0]})`;
+                    runReactor(readmeObj);
+                }
+                else throw new Error('no assets')
+            })
+            .catch(err => {
+                console.log(chalk.redBright('There\'s no asset inside the folder.\nRemember, you need to save your demo inside "Atomic_README/assets" directory.\n'));
+                getDemo();
+            })
+    };
+    
+    if (readmeObj.demo) getDemo()
+    else {
+        readmeObj.demo = '';
+        runReactor(readmeObj);
+    }
+}
+
+const getContentFunc = (user, repo, license, nick) => {
+    console.log(chalk.cyan.italic('\'Found your repo!'));
+    if (!license) console.log(chalk.yellow('Your project doesn\'t have a license! Consider adding one.'))
+
+    const questionsFunc = () => {
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    message: 'What\'s your project\'s name?',
+                    name: 'title',
+                },
+                {
+                    type: 'input',
+                    message: 'Now, write a great slogan!',
+                    name: 'slogan',
+                },
+                {
+                    type: 'input',
+                    message: 'You\'ll need a description about your project.',
+                    name: 'desc',
+                },
+                {
+                    type: 'checkbox',
+                    message: 'Which resources does your project rely on?',
+                    choices: ['JavaScript', 'Node.js', 'npm packages', 'CSS', 'HTML', 'Python'],
+                    name: 'resrc',
+                },
+                {
+                    type: 'input',
+                    message: 'You should tell people how to get started/install your product.',
+                    name: 'start',
+                },
+                {
+                    type: 'input',
+                    message: 'Now, explain how to use your product in the best way.',
+                    name: 'usage',
+                },
+                {
+                    type: 'input',
+                    message: 'Lastly, credit, where credit is due. Besides you, who would you like to acknowledge in the credits?',
+                    name: 'credits',
+                },
+                {
+                    type: 'list',
+                    message: 'Would you like to include a demo as an image or GIF?',
+                    choices: ['yes', 'no'],
+                    name: 'demo',
+                },
+            ])
+            .then(inputObj => {
+                if (license) license = true
+                const readmeObj = new ReadmeObj(user, repo, license, nick, inputObj);
+                feedReactor(readmeObj);
+            })
+    };
+
+    questionsFunc();
+};
+
+const getProfileName = (url) => { return new Promise((res, rej) => {
+    fetch(url)
+        .then(string => string.json())
+        .then(obj => res(obj.name))
+        .catch(() => console.log(chalk.redBright('Sorry, an error has been found, please start over.')))
+})};
 
 const startInquirer = () => {
-    const sloganMsg = chalk.italic('\nCreate an atomic README; now more radioactive than ever.');
+    const sloganMsg = chalk.bgBlack.italic.cyanBright('\nCreate an atomic README; now more radioactive than ever.');
     console.log(sloganMsg);
+
+    const fetchRepo = (inputObj) => {
+        const { user , repo } = inputObj;
+        const urlRepo = `https://api.github.com/repos/${user}/${repo}`;
+        const spinner = createSpinner('Looking for your repo...').start();
+
+        fetch(urlRepo)
+            .then(res => {
+                spinner.success();
+                process.stdout.moveCursor(0, -1);
+                process.stdout.clearScreenDown();
+                if (res.statusText === 'Not Found') throw new Error (res.statusText)
+                else return res.json()
+            })
+            .then(obj => {
+                const { owner: { login: userGH, url: urlUserApi }, name: repoName, license } = obj;
+                getProfileName(urlUserApi)
+                    .then(nick => getContentFunc(userGH, repoName, license, nick))
+            })
+            .catch(err => {
+                if (err.message === 'Not Found') {
+                    process.stdout.moveCursor(0, -2);
+                    process.stdout.clearScreenDown(() => console.log(chalk.redBright(`Sorry, that repo doesn\'t exist.\nPlease verify your user and repo:\n\nUser – ${user}\nRepo – ${repo}`)));
+                    setTimeout(askRepoFunc, 2000);
+                }
+            })
+    };
+
+    const askRepoFunc = () => {
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    message: 'Let\'s get started with your GitHub username:',
+                    name: 'user',
+                },
+                {
+                    type: 'input',
+                    message: 'Now your repo\'s name:',
+                    name: 'repo',
+                }
+            ])
+            .then(res => fetchRepo(res))
+    };
+
+    const triggerFunc = () => {
+        inquirer
+            .prompt(
+                {
+                    message: chalk.cyan('Press ENTER to begin...'),
+                    name: 'enterKey'
+                }
+            )
+            .then(() => {
+                process.stdout.moveCursor(0, -2);
+                process.stdout.clearScreenDown(askRepoFunc);
+            })
+    };
+
+    triggerFunc();
 }
 
 const startUpFunc = () => {
-    let startMsg = chalk.bold('\xa0README Reactor');
+    let startMsg = chalk.bold.dim('\xa0README Reactor');
     const startFx = chalkAnimation.glitch(startMsg);
+    let startFxTwo;
+    let startFxThree;
     
     setTimeout(() => {
-        startMsg = chalk.bold('\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa02.0');
-        startFx.replace(startMsg);
+        startMsg = chalk.bold.dim('\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa02.0');
+        startFx.stop();
+        process.stdout.moveCursor(0, -1);
+        process.stdout.clearScreenDown(() => {
+            startFxTwo = chalkAnimation.glitch(startMsg);
+        });
     }, 2000);
     
     setTimeout(() => {
-        startMsg = chalk.bold('\xa0README Reactor 2.0');
-        startFx.replace(startMsg);
+        startMsg = chalk.bold.dim('\xa0README Reactor 2.0');
+        startFxTwo.stop();
+        process.stdout.moveCursor(0, -1);
+        process.stdout.clearScreenDown(() => {
+            startFxThree = chalkAnimation.glitch(startMsg);
+        });
     }, 3000);
     
     setTimeout(() => {
-        startFx.stop();
+        startFxThree.stop();
         process.stdout.moveCursor(0, -1);
         process.stdout.clearScreenDown(() => console.log(startMsg));
     }, 5000);
